35880e5fef7639ef36b77f50e5bba32519f97f6b
daviskimame <hoolykim@gmail.com>
2019-10-23 17:35:20 +0300

    add firebase bucket

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask, flash, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
-from firebase_admin import credentials, storage, initialize_app
+import firebase_admin
+from firebase_admin import credentials, storage
 
 from instance.config import app_config
 from helpers.validate_file_type import allowed_file
 
 cred = credentials.Certificate('key.json')
-default_app = initialize_app(cred)
+firebase_admin.initialize_app(cred,  {
+    'storageBucket': 'plant-diseases-zd.appspot.com'
+})
+bucket = storage.bucket()
 
 UPLOAD_FOLDER = 'images'
 
@@ -33,6 +37,11 @@ def create_app(config_mode):
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            blob = bucket.blob(filename)
+            # blob.upload_from_file(file_obj=file)
+        if file and  not allowed_file(file.filename):
+           flash('please select an image')
+
     return render_template('upload.html')
 
   return app
