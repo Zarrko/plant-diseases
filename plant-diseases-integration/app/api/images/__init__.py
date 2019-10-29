07623cad9ca94622c55be83cf5d302c389131d8a
zarrko <zachariah.mwangi@imbank.co.ke>
2019-10-29 23:08:40 +0300

    fix(routes): use correct base route

--- plant-diseases-integration/app/api/images/__init__.py
+++ plant-diseases-integration/app/api/images/__init__.py
@@ -2,12 +2,12 @@
 from flask import Blueprint
 from app.__meta__ import api
 
-url = f"{api.get('version', 'v1')}/{api.get('base_route', 'crm')}"
+url = f"{api.get('version', 'v1')}/{api.get('base_route', 'plant-diseases')}"
 
 deals = Blueprint(
     name="Deals",
     import_name=__name__,
-    url_prefix=f"/api/{url}/deals",
+    url_prefix=f"/api/{url}/images",
     static_folder="static",
     template_folder="templates",
 )
